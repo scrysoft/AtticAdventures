8da23473f10760e0b41a8f18baade4a856503636
Scrysoft <admin@scrysoft-studios.com>
2024-04-19 22:05:51 +0200

    Editor Playmode Always Starts at 0

--- AtticAdventures/Assets/_Project/Script/SceneManagement/Bootstrapper.cs
+++ AtticAdventures/Assets/_Project/Script/SceneManagement/Bootstrapper.cs
@@ -1,14 +1,21 @@
 using AtticAdventures.Utilities;
+using UnityEditor.SceneManagement;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class Bootstrapper : PersistentSingleton<Bootstrapper>
 {
+    static readonly int sceneIndex = 0;
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static async void Init()
     {
         Debug.Log("Bootstrapper...");
+#if UNITY_EDITOR
+        EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[sceneIndex].path);
+#endif
         await SceneManager.LoadSceneAsync("Bootstrapper").AsTask();
     }
 }
