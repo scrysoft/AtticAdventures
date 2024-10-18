a46d4ccde008ad28201fbb55783f72a634ada8b4
Scrysoft <elmin.smilesalot@gmail.com>
2024-10-18 22:14:26 +0200

    Enemies positioned

--- AtticAdventures/Assets/_Project/Script/UI/BillboardCanvas.cs
+++ AtticAdventures/Assets/_Project/Script/UI/BillboardCanvas.cs
@@ -11,6 +11,8 @@ public class BillboardCanvas : MonoBehaviour
 
     void Update()
     {
+        if (mainCamera == null) return;
+        
         transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
                          mainCamera.transform.rotation * Vector3.up);
     }
