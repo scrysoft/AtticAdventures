68018205a881f8c66283b07420cfc17e23fa29ae
Scrysoft <elmin.smilesalot@gmail.com>
2024-12-14 05:06:46 +0100

    Gundam Arm Indicator edited

---
+++ AtticAdventures/Assets/_Project/Script/Core/SetSegmentCount.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SetSegmentCount : MonoBehaviour
+{
+    private Material material;
+
+    private void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            material = renderer.material;
+        }
+    }
+
+    public void SetSegmentCountValue(float value)
+    {
+        if (material != null)
+        {
+            int segmentCount = material.GetInt("_SegmentCount");
+            int difference = segmentCount - Mathf.RoundToInt(value);
+
+            material.SetInt("_RemovedSegments", difference);
+        }
+    }
+}
